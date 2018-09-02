e09c89544627755129e2b7d1f1a30a16b7a1f4e9
Amnhed <amnhed@MacBook-Air-de-Amnhed.local>
2018-09-02 00:50:49 -0500

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user = factory(\App\User::class)->create([
+            'name' => 'Amnhed lagunas',
+        ]);
+
+        $this->actingAs($user, 'api')
+            ->visit('api/user')
+            ->see('Amnhed');
+    }
+}
